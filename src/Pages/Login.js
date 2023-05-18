e6f73982f5dac5159f7bff59eeeba7560f617416
Matheus Fernando <matheusf.magalhaes@hotmail.com>
2023-05-18 16:36:41 -0300

    Adding login page styling, mobile first

---
+++ src/Pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../images/logo_Recipes_App.svg';
+import Tomates from '../images/tomate.svg';
+import '../Styles/Login.css';
+
+function Login() {
+  return (
+    <div id="login-content">
+      <img src={ Logo } alt="Logo" id="logo" />
+      <img src={ Tomates } alt="Tomates" id="tomates" />
+      <h1>Login</h1>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button type="button">Enter</button>
+    </div>
+  );
+}
+
+export default Login;
