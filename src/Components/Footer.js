799a42cf155a4e4c793cbfa94659a77bb9914274
Matheus Fernando <matheusf.magalhaes@hotmail.com>
2023-06-20 07:35:14 -0300

    Adding redirect on clicking footer buttons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,24 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import '../Styles/Footer.css';
 
 function Footer() {
+  const history = useHistory();
   return (
-    <footer className="footer-container">
-      <button type="button" className="drinks-btn" />
-      <button type="button" className="foods-btn" />
+    <footer className="footer-container" data-testid="footer">
+      <button
+        type="button"
+        className="drinks-btn"
+        data-testid="drinks-bottom-btn"
+        onClick={ () => history.push('/drinks') }
+      />
+      <button
+        type="button"
+        className="foods-btn"
+        data-testid="food-bottom-btn"
+        onClick={ () => history.push('/foods') }
+      />
     </footer>
   );
 }
